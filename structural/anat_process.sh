1306b0e8c2844351fc1c4a7a2ef2fd6cdb359aa5
Jae Wook Cho <jae7cho@gmail.com>
2019-11-12 21:44:53 -0500

    Update anat_process.sh

--- structural/anat_process.sh
+++ structural/anat_process.sh
@@ -15,7 +15,30 @@ num_scans=1
 gcut=false
 ## directory setup
 CCSDIR=/data3/cdb/jcho/CCS
+## if use GPU
+use_gpu=false
+## clean files
+clean_tmp_files=true
+## Fake subjects list:
+echo "jaewook" >> ${dir}/${subject}/subject.list
+subjects_list=${dir}/${subject}/subject.list
+## Template:
+standard_head=${FSLDIR}/data/standard/MNI152_T1_2mm.nii.gz
+standard_brain=${FSLDIR}/data/standard/MNI152_T1_2mm_brain.nii.gz
+standard_template=${scripts_dir}/templates/MNI152_T1_3mm_brain.nii.gz
+fsaverage=fsaverage5
 
+# Process:
+# FreeSurfer:
+bash ${CCSDIR}/H1/ccs_01_anatpreproc.sh ${subject} ${dir} ${anat} ${anat_dir_name} ${sanlm_denoised} ${num_scans} ${gcut} ${CCSDIR}
+bash ${CCSDIR}/H1/ccs_01_anatsurfrecon.sh ${subject} ${dir} ${anat} ${anat_dir_name} ${use_gpu}
 
+# Registration check:
+bash ${CCSDIR}/H1/ccs_01_anatcheck_surf.sh ${dir} ${subjects_list} ${anat_dir_name} ${clean_tmp_files}
 
-bash /data3/cdb/jcho/CCS/H1/ccs_01_anatpreproc.sh jaewook /data3/cdb/jcho jaewook.cho anat/s1 false 1 false /data3/cdb/jcho/CCS/
+## Registering to MNI152:
+bash ${CCSDIR}/H1/ccs_02_anatregister.sh ${subject} ${dir} ${anat_dir_name}
+
+## Quality assurances of spatial normalization
+reg_refine=false
+bash ${CCSDIR}/H1/ccs_02_anatcheck_fnirt.sh ${dir} ${subject_list} ${anat_dir_name} ${standard_brain} ${reg_refine}
